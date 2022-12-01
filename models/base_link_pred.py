7203112ebda5fb8c7a3428fbb7e08a1c212e2b10
Arthur Boschet <arthur.bleu25@gmail.com>
2022-12-01 12:39:02 -0500

    base template for link prediction, needs a node encoder and an edge predictor

---
+++ models/base_link_pred.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class BaseLinkPredModel(ABC):
+    '''
+    Base link prediction model template.
+    '''
+    def __init__(self):
+        '''Every link prediction models needs a node encoder (node -> vector) and edge predictor (vector -> (0,1))'''
+        super(BaseLinkPredModel, self).__init__()
+
+        #create node encoder
+        self.node_encoder = self.create_node_encoder()
+
+        #edge prediction module
+        self.edge_predictor = self.create_edge_predictor()
+
+    @abstractmethod
+    def create_node_encoder(self):
+      '''Creates the node encoder'''
+      pass
+
+    @abstractmethod
+    def create_edge_predictor(self):
+      '''Creates the edge predictor'''
+      pass
