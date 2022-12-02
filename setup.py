f508f5dde3ef95323d5ee2be91de40595f7c7258
Arthur Boschet <arthur.bleu25@gmail.com>
2022-12-02 14:17:59 -0500

    testing pip setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="linkpred",
+    # version="0.1.2.3",
+    # author="Author Name",
+    # author_email="your_mail@adress.com",
+    # packages=["module_1", "module_2"],
+    description="An awesome package that does something",
+)
