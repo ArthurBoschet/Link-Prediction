6d7ce041889ac2cf1e44ec97d1e8fe10dde31dc7
Arthur Boschet <arthur.bleu25@gmail.com>
2022-12-02 14:57:13 -0500

    added packages/author to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="linkpred",
-    # version="0.1.2.3",
-    # author="Author Name",
+    version="0.0.0.1",
+    author="Arthur Boschet",
     # author_email="your_mail@adress.com",
-    # packages=["module_1", "module_2"],
-    description="An awesome package that does something",
+    packages=find_packages(),
+    description="link prediction package",
 )
