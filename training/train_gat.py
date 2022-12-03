a0408060ba23ae5ea7f28ad5691d2e1fa3c625d3
Arthur Boschet <arthur.bleu25@gmail.com>
2022-12-02 21:26:35 -0500

    updated train_gat so that the dataloaders are outside of it (memory error with ray tune)

--- training/train_gat.py
+++ training/train_gat.py
@@ -1,44 +1,23 @@
-import torch
-from torch.utils.data import DataLoader
-
 from linkpred.gat_link_pred import GATLinkPredModel
 from training.train import train
 from training.train_gnn import train_step_GNN, model_predict_GNN
 
+import torch
 
-from deepsnap.batch import Batch
-
+from ray.air import session
 from ray.tune.integration.wandb import (
     wandb_mixin,
 )
 
 @wandb_mixin
-def train_gat(config ,datasets=None):
-
-  assert datasets is not None, 'Data set should be passed as an argument'
-
-  config = {
-    "device": 'cuda' if torch.cuda.is_available() else 'cpu',
-    "epochs": 3,
-    "hidden_dim": 128, 
-    "num_layers_gat": 2, 
-    "num_layers_mlp": 1, 
-    "heads": 3, 
-    "dropout_gat" : 0, 
-    "gat_type" : 'v2', 
-    "dropout": 0,
-    "lr": 0.01,
-    "momentum": 0.9,
-    "weight_decay": 5e-4,
-  }
+def train_gat(config, dataloaders_id):
 
   #print which device is being used to the terminal
   device = config["device"]
   print(f"\nThe device is {device}\n")
 
   #get number of input dims and classes
-  input_dim = 2 #datasets['train'].num_node_features
-  num_classes = 2 #datasets['train'].num_edge_labels
+  input_dim = 2 
 
   #GAT link prediction model
   model = GATLinkPredModel( 
@@ -58,11 +37,7 @@ def train_gat(config ,datasets=None):
   #optimizer setup
   optimizer = torch.optim.SGD(model.parameters(), lr=config['lr'], momentum=config['momentum'], weight_decay=config['weight_decay'])
 
-  #setup dataloader
-  dataloaders = {split: DataLoader(
-              ds, collate_fn=Batch.collate([]),
-              batch_size=1, shuffle=(split=='train'))
-              for split, ds in datasets.items()}
-
   #training
-  return train(model, train_step_GNN, model_predict_GNN, dataloaders, optimizer, config, save_best=False, verbose=True)
+  score_val = train(model, train_step_GNN, model_predict_GNN, dataloaders_id, optimizer, config, save_best=False, verbose=True)
+  session.report({"auroc_validation": score_val})
+  return
