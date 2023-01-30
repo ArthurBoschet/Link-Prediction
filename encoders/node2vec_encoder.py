ab39b7b62ba4926bcc3297b66aad79b6232cd4e8
Arthur Boschet <arthur.bleu25@gmail.com>
2023-01-29 22:02:29 -0500

    node2vec first version added

---
+++ encoders/node2vec_encoder.py
@@ -0,0 +1,93 @@
+from node2vec import Node2Vec
+from gensim.models import Word2Vec
+import networkx as nx
+import torch
+import os
+
+class FastEmbedder():
+  def __init__(self, keyed_vectors):
+    self.keyed_vectors = keyed_vectors
+
+  def _embed(self, right_nodes, left_nodes):
+    return self.keyed_vectors[right_nodes], self.keyed_vectors[left_nodes]
+
+  def __getitem__(self, edges):
+    if edges.ndim > 1:
+      right_nodes = edges[:,0]
+      left_nodes = edges[:,1]
+    else:
+      right_nodes = edges[0]
+      left_nodes = edges[1]
+    return self._embed(right_nodes, left_nodes)
+
+
+class Node2VecEncode(torch.nn.module):
+    def __init__(
+        self,
+        graph,
+        input_size,
+        walk_length,
+        p,
+        q,
+        directory,
+        species,
+        ):
+        '''
+        Node2vec encoder
+        Parameters:
+            graph (deepsnap.graph.Graph): DeepSnap training graph used for embedding the nodes
+            input_size (int): size of the input vector
+            walk_length (int): length of the random walks
+            p (float): return parameter
+            q (float): inout parameter
+            directory (str): the directory where the node2vec embeddings are stored
+            species (str): the species evaluated
+
+        '''
+        super(Node2VecEncode, self).__init__()
+
+         #create directory
+        os.makedirs(directory, exist_ok=True) 
+
+        #get node2vec directory
+        n2v_dir = directory + f"/{species}_embedding_model_dim:{input_size}_len:{walk_length}_p:{p}_q:{q}"
+
+        #try to load the model from the directory if it already exists 
+        try:
+            model = Word2Vec.load(n2v_dir)
+
+        #if the model doesn't exist then create the model and save it
+        except:
+            G = nx.Graph()
+            G.add_nodes_from(list(graph.node_label_index.numpy()))
+            G.add_edges_from(self.get_edge_list(graph))
+
+            #generate walks
+            node2vec = Node2Vec(G, dimensions=self.input_size, walk_length=self.walk_length, num_walks=50, workers=1, p=self.p, q=self.q)
+
+            #train node2vec model
+            n2w_model = node2vec.fit(window=7, min_count=1)
+
+            #save model for later use
+            n2w_model.save(n2v_dir)
+
+            #get word2vec model
+            model = Word2Vec.load(n2v_dir)
+
+        #instanciate the edge embedder
+        self.embedder = FastEmbedder(keyed_vectors=model.wv)
+
+
+    def get_edge_list(self, graph):
+        '''
+        Get the edge list for the deepsnap graph
+        Parameter:
+            graph (deepsnap.graph.Graph): DeepSnap training graph used for embedding the nodes
+        Return:
+            List(Tuple(str, str)): edge list
+        '''
+        return [tuple(e) for e in graph.edge_index.numpy().T]
+
+    
+    def forward(self, e):
+        return self.embedder[e]
---
+++ linkpred/node2vec_link_pred.py
@@ -0,0 +1,152 @@
+from linkpred.base_link_pred import BaseLinkPredModel
+from predictors.mlp_predictor import MultiLayerPerceptron
+from predictors.dot_product_predictor import DotProduct
+from training.trainable import TrainableModel
+from node2vec import Node2Vec
+from gensim.models import Word2Vec
+import networkx as nx
+import torch
+import os
+
+
+class Node2VecLinkPredModel(BaseLinkPredModel, TrainableModel, torch.nn.Module):
+    '''
+    Node2vec prediction model that uses node2vec as the node encoder while using a neural network as the predictor.
+    '''
+    def __init__(
+        self, 
+        G,
+        input_size,
+        walk_length,
+        p,
+        q, 
+        num_layers, 
+        hidden_size,
+        device,
+        dropout=0,
+        directory='drive/MyDrive/protein_network/string/human/n2v_embeddings',
+        species='human',
+        ):
+      '''
+      Sets up a binary cross entropy loss function combined with final sigmoid layer for added numerical stability. 
+      The node2vec model is also fitted during the instanciation
+      Parameters:
+        G (deepsnap.graph.Graph): DeepSnap training graph used for embedding the nodes
+        input_size (int): size of the input vector
+        walk_length (int): length of the random walks
+        p (float): return parameter
+        q (float): inout parameter
+        hidden size (int): size of the hidden layers
+        device (torch.cuda.device or str): device used by the model (gpu or cpu)
+        num_layers (int): number of MLP layers (must be >= 0 and if 0 then dot product of the two nodes embeddings)
+        dropout (float): probability of dropout at each MLP layer during training
+        directory (str): the directory where the node2vec embeddings are stored
+        species (str): the species evaluated
+      '''
+
+      #attributes for embedding directory
+      self.directory = directory
+      self.species = species
+
+      #predictor network
+      self.num_layers = num_layers
+      self.hidden_size = hidden_size
+      self.dropout = dropout
+
+      #encoder node2vec
+      self.walk_length = walk_length
+      self.p = p
+      self.q = q
+      self.G = G
+      self.input_size = input_size
+      self.walk_length = walk_length
+
+      #device
+      self.device = device
+
+      super(Node2VecLinkPredModel, self).__init__()
+
+      #combines sigmoid and BCE into one function but is more numerically stable
+      self.loss_fn = torch.nn.BCEWithLogitsLoss()
+
+
+    def create_node_encoder(self):
+      return
+
+
+    def create_edge_predictor(self):
+      if self.num_layers > 0:
+        return MultiLayerPerceptron(self.input_size, self.hidden_size, self.num_layers, dropout=self.dropout)
+      else:
+        return DotProduct()
+
+
+    def loss(self, pred, label):
+      return self.loss_fn(pred, label)
+
+    
+    def forward(self, batch):
+      '''
+      get node2vec embeddings for encoding the nodes followed by edge predictions
+      Parameters:
+         batch (deepsnap.batch.Batch): batch with the message passing edges, supervision edges and node features
+
+      Returns:
+         pred (torch.tensor)
+      '''
+      edge_label_index = batch.to('cpu').numpy().T
+
+      #create node embeddings with node2vec
+      nodes_first, nodes_second = self.node_encoder(edge_label_index)
+
+      #convert to pytorch
+      nodes_first = torch.from_numpy(nodes_first).to(self.device)
+      nodes_second = torch.from_numpy(nodes_second).to(self.device)
+
+      #predict edges from two nodes
+      pred = self.edge_predictor(nodes_first,  nodes_second)
+
+      return pred
+
+
+    def train_step(self, data, optimizer):
+      '''training step function for model (for each batch)'''
+
+      #set the batch to the device (cpu or gpu)
+      data.to(self.device)
+
+      batches_edges = torch.split(data.edge_label_index, self.batch_size, dim=1)
+      batches_targets = torch.split(data.edge_label, self.batch_size, dim=0)
+
+      for i, (batch, edge_label) in enumerate(zip(batches_edges, batches_targets)):
+
+        #set the model to training mode
+        self.train()
+
+        #no gradient accumulation
+        optimizer.zero_grad()
+
+        #get predictions from training batch
+        pred = self.forward(batch)
+
+        #get loss
+        loss = self.loss(pred, edge_label.type(pred.dtype))
+
+        #gradient descent step
+        loss.backward()
+        optimizer.step()
+
+      return loss
+
+
+    def predict(self, batch):
+      '''Prediction function has to be implemented'''
+      #set the model to eval mode
+      self.eval()
+
+      #send batch to proper device and evaluate predictions
+      edge_label_index = batch.edge_label_index
+      pred = self.forward(edge_label_index)
+      pred = torch.sigmoid(pred)
+
+      return pred
