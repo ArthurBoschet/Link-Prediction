b335ef48c91bc7eaa3ad164185741e099945f7e7
Arthur Boschet <arthur.bleu25@gmail.com>
2023-02-08 14:09:42 -0500

    update the train node2vec function

--- training/train_node2vec.py
+++ training/train_node2vec.py
@@ -1,8 +1,11 @@
 from linkpred.node2vec_link_pred import Node2VecLinkPredModel
 from training.train import train
 
+#Pytorch
 import torch
 
+#ray
+import ray
 from ray.air import session
 from ray.tune.integration.wandb import (
     wandb_mixin,
@@ -15,9 +18,12 @@ def train_node2vec(config):
   device = config["device"]
   print(f"\nThe device is {device}\n")
 
+  datasets = ray.get(config['datasets_id'][config["seed"]])
+
   #GAT link prediction model
   model = Node2VecLinkPredModel( 
-    config["graph"],
+    datasets['train'][0],
+    config["seed"],
     int(config["input_size"]),
     int(config["walk_length"]),
     config["p"],
@@ -38,6 +44,7 @@ def train_node2vec(config):
   optimizer = torch.optim.SGD(model.parameters(), lr=config['lr'], momentum=config['momentum'], weight_decay=config['weight_decay'])
 
   #training
-  score_val = train(model, config['dataloaders_id'], optimizer, config, save_best=False, verbose=True)
+  score_val = train(model, config['dataloaders_id'][config["seed"]], optimizer, config, verbose=True)
   session.report({"auroc_validation": score_val})
+  print(score_val)
   return
